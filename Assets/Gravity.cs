420028303862b92c0d356d8e7df8e40dcc172345
JedchadapornKumpo <Jedchadaporn.kump@bumail.net>
2025-03-11 01:37:49 +0700

    ดาวทุกดวงเข้าหาดวงอาทิตย์

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -1,7 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Gravity : MonoBehaviour
 {
+    Rigidbody rb;
+    const float G = 0.006674f;//Gravitational Constant 6.674
+    
+    //create a List of objects in the galaxy to attract
+    public static List<Gravity> planetLists;
+   
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (planetLists == null)
+        {
+            planetLists = new List<Gravity>();
+        }
+
+        planetLists.Add(this);
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var planet in planetLists)
+        {
+            if(planet != this)
+               Attract(planet);
+        }
+    }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        
+        float forceMagnitude = G * ((rb.mass * otherRb.mass)/Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+        otherRb.AddForce(finalForce);
+
+    }
+   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
